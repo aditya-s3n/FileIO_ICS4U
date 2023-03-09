7b82e29394f746bb5ca994c7cacee6957988a582
Aditya Sen <adityasen120@gmail.com>
2023-03-08 19:54:59 -0500

    add csv files

---
+++ sun_data_structures.py
@@ -0,0 +1,14 @@
+import csv
+
+def get_all_data():
+    pass
+
+def time_series_analysis():
+    pass
+
+def correlation_sunspots_flare():
+    pass
+
+def main():
+    all_flares = []
+    all_sunspots = []
